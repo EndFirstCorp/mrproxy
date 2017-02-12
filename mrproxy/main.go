1b8cd3afcf988c34b85815d802f80b684eb2ee5a
Rob Archibald <rob.archibald@endfirst.com>
2017-02-11 21:29:01 -0800

    use config file for settings, not command-line since we need to pass in password as well and that can't be passed through command-line

--- mrproxy/main.go
+++ mrproxy/main.go
@@ -3,49 +3,51 @@ package main
 import (
 	"bufio"
 	"flag"
-	"github.com/garyburd/redigo/redis"
-	"github.com/zobo/mrproxy/protocol"
-	"github.com/zobo/mrproxy/proxy"
-	"github.com/zobo/mrproxy/stats"
+	"fmt"
 	"log"
 	"net"
 	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/robarchibald/configReader"
+	"github.com/zobo/mrproxy/protocol"
+	"github.com/zobo/mrproxy/proxy"
+	"github.com/zobo/mrproxy/stats"
 )
 
 var version string
 
-// "10.13.37.106:6379"
-var redis_server = flag.String("server", "127.0.0.1:6379", "Redis server to connect to")
-var listen_addr = flag.String("bind", "0.0.0.0:11211", "Bind address and port")
+var configFile = flag.String("c", "/etc/redis/mrproxy.conf", "Config file location for mrproxy")
+var logFile = flag.String("l", "/var/log/mrproxy.log", "Log file for mrproxy")
+
+type proxyConfig struct {
+	redisServer         string
+	redisPort           int
+	redisPassword       string
+	redisMaxIdle        int
+	redisMaxConnections int
+	redisTimeoutSeconds int
+	bindIPAddress       string
+	bindPort            int
+}
 
 func main() {
-
 	flag.Parse()
-
-	// move to global??
-	pool := &redis.Pool{
-		MaxIdle:     3,
-		IdleTimeout: 240 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			//c, err := redis.Dial("tcp", *redis_server)
-			d, _ := time.ParseDuration("1s")
-			c, err := redis.DialTimeout("tcp", *redis_server, d, d, d)
-			if err != nil {
-				return nil, err
-			}
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
-
-	l, err := net.Listen("tcp", *listen_addr)
+	c := proxyConfig{}
+	err := configReader.ReadFile(*configFile, &c)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Printf("Listening %v", l)
+
+	redis := fmt.Sprintf("%s:%d", c.redisServer, c.redisPort)
+	pool := newConnPool(redis, c.redisPassword, c.redisMaxIdle, c.redisMaxConnections, c.redisTimeoutSeconds)
+
+	bind := fmt.Sprintf("%s:%d", c.bindIPAddress, c.bindPort)
+	l, err := net.Listen("tcp", bind)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("Proxying Memcache from %s to Redis at %s", redis, bind)
 	for {
 		c, err := l.Accept()
 		if err != nil {
@@ -53,6 +55,53 @@ func main() {
 		}
 		go processMc(c, pool)
 	}
+}
+
+func newConnPool(address string, password string, maxIdle, maxConnections, timeoutSeconds int) *redis.Pool {
+	timeout := time.Duration(timeoutSeconds) * time.Second
+	return &redis.Pool{
+		MaxIdle:   maxIdle,
+		MaxActive: maxConnections,
+		Dial: func() (redis.Conn, error) {
+			tc, err := dial("tcp", address)
+			if err != nil {
+				return nil, err
+			}
+			c := redis.NewConn(tc, timeout, timeout)
+			if password != "" {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func dial(network, addr string) (net.Conn, error) {
+	tcpAddr, err := net.ResolveTCPAddr(network, addr)
+	if err != nil {
+		return nil, err
+	}
+	tc, err := net.DialTCP(network, nil, tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+	if err := tc.SetKeepAlive(true); err != nil {
+		return nil, err
+	}
+	if err := tc.SetKeepAlivePeriod(2 * time.Minute); err != nil {
+		return nil, err
+	}
+	return tc, nil
 }
 
 func processMc(c net.Conn, pool *redis.Pool) {
@@ -84,22 +133,11 @@ func processMc(c net.Conn, pool *redis.Pool) {
 			log.Printf("%v ReadRequest err: %v", c, err)
 			return
 		}
-		log.Printf("%v Req: %+v\n", c, req)
 
-		switch req.Command {
-		case "quit":
-			return
-		case "version":
-			res := protocol.McResponse{Response: "VERSION mrproxy " + version}
+		res := proxy.Process(req)
+		if !req.Noreply {
 			bw.WriteString(res.Protocol())
 			bw.Flush()
-		default:
-			res := proxy.Process(req)
-			if !req.Noreply {
-				log.Printf("%v Res: %+v\n", c, res)
-				bw.WriteString(res.Protocol())
-				bw.Flush()
-			}
 		}
 	}
 }
